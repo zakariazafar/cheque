762a5305839ad6c332fbd6991693c57234bc2617
zakariazafar <zakariazafar@yahoo.com>
2016-02-25 10:59:13 +1100

    browser acceptence test added for search filter

--- app/assets/javascripts/controllers/ChequeController.js
+++ app/assets/javascripts/controllers/ChequeController.js
@@ -5,22 +5,22 @@ controllers = angular.module('controllers',[]);
 controllers.controller("ChequeController", [
   '$scope','$routeParams','$location', function($scope,$routeParams,$location) {
     
-		$scope.cheques =  [
+		cheques =  [
   {
     id: 1,
-    name: "Test Name 1",
+    name: "Test Name1",
     date: "2016-02-10",
     value: 123456
   },
   {
     id: 2,
-    name: "Test Name 2",
+    name: "Test Name2",
     date: "2016-02-11",
     value: 560
   },
   {
     id: 3,
-    name: "Test Name 3",
+    name: "Test Name3",
     date: "2016-02-12",
     value: 1245
   },
@@ -30,7 +30,31 @@ controllers.controller("ChequeController", [
      date: "2016-02-13",
     value: 678
   },
-];;
+  {
+    id: 4,
+    name: "Test Name2",
+     date: "2016-02-15",
+    value: 900
+  }
+];
+
+   $scope.cheques = cheques;
+
+   $scope.search = function(keyword){
+    if (keyword != 'all') {
+    $scope.cheques = cheques.filter(function(cheque) {
+          return cheque.name.toLowerCase() == keyword.toLowerCase();
+       });
+   }
+   else
+    $scope.cheques = cheques;
+   
+   }
+
+   
+
+  
+
 
   }
 ]);
