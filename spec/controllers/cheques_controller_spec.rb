67a1dac9782d60a7052a1b4fb9c82c5ec43454bc
zakariazafar <zakariazafar@yahoo.com>
2016-02-25 12:27:43 +1100

    test added for cheques_controller

--- spec/controllers/cheques_controller_spec.rb
+++ spec/controllers/cheques_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe ChequesController do
+  render_views
+  describe "index" do
+	  before do
+	      Cheque.create!(name: 'Test Name1',date: '2016-02-10',amount: 123456)
+	      Cheque.create!(name: 'Test Name2',date: '2016-02-11',amount: 560)
+	      Cheque.create!(name: 'Test Name3',date: '2016-02-12',amount: 1245)
+	      Cheque.create!(name: 'Test Name4',date: '2016-02-13',amount: 678)
+	      Cheque.create!(name: 'Test Name2',date: '2016-02-15',amount: 900)
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+	      xhr :get, :index, format: :json
+	  end
+ 	subject(:results) { JSON.parse(response.body) }
+
+    def extract_name
+      ->(object) { object["name"] }
     end
   end
 
