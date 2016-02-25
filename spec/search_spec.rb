23143f1ab80cfd3287fe5c52726e256718153e94
zakariazafar <zakariazafar@yahoo.com>
2016-02-25 11:00:12 +1100

    browser acceptence test added for search filter

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -5,9 +5,11 @@ feature "Looking up cheques", js: true do
   scenario "finding cheques by name" do
     visit '/'
     
-     first("a", "Test Name2").click
-    
+    # first("a", "Test Name2").click
+     click_link('Test Name2', match: :first)
+    # click_to find("a", :text => "Test Name2")
+    #first(find("a", :text => "Test Name2")).click
     expect(page).to have_content("560")
-    expect(page).to have_no_content("1250")
+    expect(page).to have_no_content("900")
   end
 end
